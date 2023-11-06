c72b6675945c581646a8851f6792d380135c7e6f
mariade_snchz <calder07@gmail.com>
2023-11-06 19:24:43 -0300

    cors

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 
         try {
 
-            const response = await axios.post('https://pdf-backend-hmiprdxvp-mariadesanchez.vercel.app/pdfviewer', formData, {
+            const response = await axios.post('https://pdf-backend-15orodozp-mariadesanchez.vercel.app/pdfviewer', formData, {
 
 
 
